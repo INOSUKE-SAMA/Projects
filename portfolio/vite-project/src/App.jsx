855690d0dcec34ccf39d9f9c780f9109b271182e
Chirag shahi <shahichirag10@gmail.com>
2025-01-02 19:16:05 +0545

    Portfolio Day3: About and Experience

--- portfolio/vite-project/src/App.jsx
+++ portfolio/vite-project/src/App.jsx
@@ -1,11 +1,16 @@
+import About from "./components/About/About"
 import Navbar from "./components/navbar/navbar"
 import Home from "./components/Navbar/Home/Home"
+import Experience from "./components/Experience/Experience"
+
 
 function App(){
       return(
         <div className='bg-[#171d32] h-auto w-full overflow-hidden'>
           <Navbar/>
           <Home/>
+          <About/>
+          <Experience/>
         </div>
       )
 }
