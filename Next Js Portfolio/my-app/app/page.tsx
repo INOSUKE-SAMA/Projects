523ab6cb7daa5e31ab6460beed4d7cc4852687ad
INOSUKE-SAMA <shahichirag10@gmail.com>
2025-03-03 17:42:19 +0545

    Next js

---
+++ Next Js Portfolio/my-app/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <main >
+    <div className="">
+      <h1>Hello Portfolio</h1>
+    </div>
+    <h1 className="bg"></h1>
+   </main>
+  );
+}
